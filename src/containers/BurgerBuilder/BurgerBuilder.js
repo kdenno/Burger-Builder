770a51719258242f34c376a85e1f1cf20a2a639d
kdenno <kabuyedennis3@gmail.com>
2020-03-18 16:26:29 +0300

    project setup

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>BurgerBuilder</div>
+        <div>BurgerBuilder Controls</div>
+      </Aux>
+    );
+  }
+}
+export default BurgerBuilder;
