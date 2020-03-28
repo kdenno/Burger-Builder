125df2f5d5cd9ea01b7a5edc20a1faa0baeb2d71
kdenno <kabuyedennis3@gmail.com>
2020-03-28 12:26:05 +0300

    add routint with react-router-dom

---
+++ src/components/Order/CheckoutSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Burger from "../Burger/Burger";
+import Button from "../UI/Button/Button";
+import styles from "./CheckoutSummary.module.css";
+
+const checkoutSummary = props => {
+  return (
+    <div className={styles.CheckoutSummary}>
+      <h1>We hope it tastes well</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button type="Danger" clicked>
+        Cancel
+      </Button>
+      <Button type="Success" clicked>
+        Buy Now
+      </Button>
+    </div>
+  );
+};
+export default checkoutSummary;
