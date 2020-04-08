982a2be3890b1a0727627d9228a85e43ee7c7fdc
kdenno <kabuyedennis3@gmail.com>
2020-04-08 19:04:26 +0300

    add burger builder action creators

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,18 @@
+// burger builder action creators
+import * as actionTypes from "./actionTypes";
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    }
+
+}
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    }
+
+}
