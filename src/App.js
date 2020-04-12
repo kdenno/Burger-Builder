6a0a5606d38094c183b5a889ec8250a350301d2c
kdenno <kabuyedennis3@gmail.com>
2020-04-12 13:42:59 +0300

    persist auth status with Local Storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { Component } from 'react';
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
-import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </div>
-  );
+// always check state of Auth
+import { connect } from "react-redux";
+import * as Actions from "./store/actions/index";
+import { Route, Switch, withRouter } from "react-router-dom";
+
+class App extends Component{
+  componentDidMount () {
+    this.props.onTryAutoLogin();
+  }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoLogin: () => dispatch(Actions.checkAuthState()),
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App)); // connect on the main component drowns out the react router so we employ withRouter a higher order react component and wrap it around connect
